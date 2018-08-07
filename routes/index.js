54f66e1e6d6b9ac831b986aabfd842458c10fe58
cannblw <cannblw@github>
2018-08-07 19:28:55 +0200

    Create route to handle urlIDs and redirect the client to the desired website.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+var knex = require('../knex');
+
 /* GET home page. */
 router.get('/', (req, res) => {
     res.render('index', {
         baseUrl: process.env.base_url
     });
+});
+
+router.get('/:urlID', (req, res) => {
+    const urlID = req.params.urlID;
+
+
+
+    // Get corresponding URL from DB
+    knex('urls').select('url').where('url_id', urlID).first().then(data => {
+        if(data && data != null) {
+            return res.redirect(data.url);
+        }
+
+        res.status(404).send('Not found');
+    });
+
 });
 
 module.exports = router;
